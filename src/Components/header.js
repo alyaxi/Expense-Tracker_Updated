152ad5a1e2f66975790b51c77e2492edc620b637
Syed Ali Uz Zaman <syedaliuzzaman@gmail.com>
2020-08-18 19:58:08 -0700

    Final Updated

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const Header = () => {
     return(
+        <div>
         <h2>
             Expense Tracker 
         </h2>
+        <p>Created by Syed Ali Uz Zaman</p>
+        </div>
     )
 }
